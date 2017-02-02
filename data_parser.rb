4a951dfc2ec5af6c7cc228830e5045d29ddd767a
Ben Call <bgcall@indiana.edu>
2017-02-02 17:36:11 -0500

    First updated code

---
+++ data_parser.rb
@@ -0,0 +1,46 @@
+require 'csv'
+
+class Delivery
+  attr_accessor :destination, :shipped, :number, :money, :pilot
+
+  def initialize (destination:, what_got_shipped:, number_of_crates:, money_we_made:)
+    @destination=destination
+    @shipped=what_got_shipped
+    @number=number_of_crates
+    @money=money_we_made
+    @pilot=change_pilot
+  end
+
+  def change_pilot
+      pilot = case destination
+              when "Earth" then "Fry"
+              when "Mars" then "Amy"
+              when "Uranus" then "Bender"
+              else "Leela"
+              end
+  end
+end
+
+
+blank_array = []
+
+data = CSV.foreach("planet_express_logs.csv", headers: true, header_converters:
+:symbol, converters: :all) do |row|
+                             blank_array << Delivery.new(row)
+                           end # replace with your logic
+
+puts blank_array.inspect
+
+sum = blank_array.inject(0){|cash, total| cash + total.money}
+
+puts sum
+
+puts blank_array.inspect
+#First try at summing total money made
+# total_money = []
+# CSV.foreach("planet_express_logs.csv", headers: true, header_converters:
+# :symbol, converters: :all) do |row|
+#   total_money << row[:money_we_made]
+# end
+#
+# puts total_money.inject(0, &:+).inspect
