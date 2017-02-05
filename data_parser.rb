174fc502f9b02adf5d2b457be16b35d0a2600ce2
Ben Call <bgcall@indiana.edu>
2017-02-05 17:21:30 -0500

    Final Weekend Lab

--- data_parser.rb
+++ data_parser.rb
@@ -1,4 +1,8 @@
+#!/usr/bin/env ruby
+
 require 'csv'
+
+file_name = ARGV[0]
 
 class Delivery
   attr_accessor :destination, :shipped, :number, :money, :pilot, :bonus
@@ -14,10 +18,10 @@ class Delivery
 
   def change_pilot
       pilot = case destination
-              when "Earth" then "Fry"
-              when "Mars" then "Amy"
-              when "Uranus" then "Bender"
-              else "Leela"
+                when "Earth" then "Fry"
+                when "Mars" then "Amy"
+                when "Uranus" then "Bender"
+                else "Leela"
               end
   end
 
@@ -45,64 +49,39 @@ attr_accessor :file
     end
   end
 
+  def test_output (driver)
+    "#{driver} flew " + trips(driver) + "times and made " + bonus(driver) + "dollars in bonus!"
+  end
+
+  def self.money_this_week
+    file(:money_we_made).inject(0, &:+).inspect
+  end
+
   def trips(driver)
     file.select{|flyist| flyist.pilot==driver}.count
+  end
+
+  def bonus(driver)
+    file.select{|flyist| flyist.pilot==driver}.collect(&:bonus).inject(:+)
+  end
+
+  def planet_tracker(driver)
+    file.select{|flyist| flyist.pilot==driver}.collect(&:destination).uniq.join("")
   end
 end
 
 csv_file = "planet_express_logs.csv"
 
 tracker=Parse.new
-tracker.parse_data(csv_file)
-puts tracker.trips("Fry")
+tracker.parse_data(file_name)
+tracker.trips("Fry")
+tracker.bonus("Fry")
+tracker.planet_tracker("Fry")
 
-# blank_array = []
-#
-# data = CSV.foreach("planet_express_logs.csv", headers: true, header_converters:
-# :symbol, converters: :all) do |row|
-#                              blank_array << Delivery.new(row)
-#                            end # replace with your logic
-#
-# sum = blank_array.inject(0){|cash, total| cash + total.money}
-#
-# puts sum
-#
-# fry_trips = blank_array.select{|pilot_name| pilot_name.pilot? "Fry"}.collect(&:destination).count
-# fry_planet = blank_array.select{|pilot_name| pilot_name.pilot? "Fry"}.collect(&:destination).uniq.join("")
-# fry_bonus = blank_array.select{|pilot_name| pilot_name.pilot? "Fry"}.collect(&:bonus).inject(:+)
-#
-# puts "Fry took #{fry_trips} trips to #{fry_planet} and made #{fry_bonus} in bonus money"
-#
-# amy_trips = blank_array.select{|pilot_name| pilot_name.pilot? "Amy"}.collect(&:destination).count
-# amy_planet = blank_array.select{|pilot_name| pilot_name.pilot? "Amy"}.collect(&:destination).uniq.join("")
-# amy_bonus = blank_array.select{|pilot_name| pilot_name.pilot? "Amy"}.collect(&:bonus).inject(:+)
-#
-# puts "Amy took #{amy_trips} trip to #{amy_planet} and made #{amy_bonus} in bonus money"
-#
-# bender_trips = blank_array.select{|pilot_name| pilot_name.pilot? "Bender"}.collect(&:destination).count
-# bender_planet = blank_array.select{|pilot_name| pilot_name.pilot? "Bender"}.collect(&:destination).uniq.join("")
-# bender_bonus = blank_array.select{|pilot_name| pilot_name.pilot? "Bender"}.collect(&:bonus).inject(:+)
-#
-# puts "Bender took #{bender_trips} trips to #{bender_planet} and made #{bender_bonus} in bonus money"
-#
-# leela_trips = blank_array.select{|pilot_name| pilot_name.pilot? "Leela"}.collect(&:destination).count
-# leela_planet = blank_array.select{|pilot_name| pilot_name.pilot? "Leela"}.collect(&:destination).uniq.join(", ")
-# leela_bonus = blank_array.select{|pilot_name| pilot_name.pilot? "Leela"}.collect(&:bonus).inject(:+)
-#
-# puts "Leela took #{leela_trips} trips to the planets #{leela_planet} and made #{leela_bonus} in bonus money"
-#
-# #Pilot array
-# pilot_array = blank_array.collect(&:pilot).uniq.inspect
-# puts pilot_array
-#
-# puts blank_array.trips("Fry")
+profit=tracker.file.collect(&:money).inject(:+)
+pilots=tracker.file.collect(&:pilot).uniq
 
+puts "#{profit}: total made"
 
-#First try at summing total money made
-# total_money = []
-# CSV.foreach("planet_express_logs.csv", headers: true, header_converters:
-# :symbol, converters: :all) do |row|
-#   total_money << row[:money_we_made]
-# end
-#
-# puts total_money.inject(0, &:+).inspect
+pilots.each{|pilot| puts "#{pilot} made #{tracker.trips(pilot)} trips"}
+pilots.each{|pilot| puts "#{pilot} made #{tracker.bonus(pilot)} dollars in bonus"}
